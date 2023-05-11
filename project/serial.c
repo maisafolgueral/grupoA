8ff40cb2ce27c845ef12ca9f61a71939b2addfb8
Ubuntu <costapfilipe@gmail.com>
2023-05-11 02:05:16 +0000

    Updated serial

--- project/serial.c
+++ project/serial.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
+#include <gmp.h>
 
-long double fat(int n) {
-  long double fat = 1;
-  for(int i = 1; i <= n; i++) {
-    fat = fat * i;
-  }
-  return fat;
-}
-
-long double euler(int nMax) {
-  long double soma = 0;
-  for (int n = 0; n <= nMax; n++) {
-    soma += 1 / fat(n);
-  }
-  return soma;
-}
-
-int main(void) {
-  long double resultado = euler(20);
-  printf("%.62Lf \n", resultado);
+int main()
+{
+    // Set pointer to a file
+    FILE *fp;
   
-  return 0;
+    // Start and end of calculus
+    int n, nMax; 
+
+    // Define variables of GMP lib to do the calculus
+    mpf_t euler, division, factorial;
+
+    // Precision set to unsigned int => {number}{unsigned = U}
+    mpf_set_default_prec(5000000U);
+  
+    // Initialize the variables to do the calculus
+    mpf_init_set_ui(euler, 1); // Set 1 to euler
+    mpf_init(division);
+    mpf_init_set_ui(factorial, 1); // Set 1 to factorial
+
+    // Calculate Euler constant
+    for(n = 1, nMax = 100000; n <= nMax; n++) 
+    {
+      // Calculate n!
+      mpf_mul_ui(factorial, factorial, n);
+
+      // Calculate 1/n!
+      mpf_ui_div(division, 1, factorial);
+
+      // Add the result of 1/n! to euler
+      mpf_add(euler, euler, division);
+    }
+
+    // Write the result to a file
+    fp = fopen("output.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    gmp_fprintf(fp, "%.1000000Ff\n", euler);
+    fclose(fp);
+ 
+    /*
+    Below, free memory allocated to variables
+    
+    This is used, because GMP allocates memory in heap. 
+    Then, we need to free this memory.
+    */
+    mpf_clear(euler);
+    mpf_clear(division);
+    mpf_clear(factorial);
+
+    return 0;
 }
